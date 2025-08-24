75d6e78c40089ecbdebd3eef941bc850064368e9
kafuuchino4302 <79404838+kafuuchino4302@users.noreply.github.com>
2025-08-24 14:09:01 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-export const API_KEY = 'xai-YaOdm7OMDEJ5uS0iQcrWoKxAjdsB5VmadSjSw0rgCkI7THN5AA6Ss1s51CkpVV1Y3jxlttPHxSdtwvlv';
-
 export const systemPrompts = {
     standard: `你将扮演一名专注于胸部分析的资深角色概念设计师。你的唯一任务是接收一张二次元角色立绘，并估算出其最可能的罩杯尺寸。
     {
